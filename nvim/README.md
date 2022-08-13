b2d174ca4ce9e7842131daab675909cd213c042a
4n4mn3s1s <sergio.sergio92@gmail.com>
2022-08-13 18:47:18 +0200

    .

--- nvim/README.md
+++ nvim/README.md
@@ -7,6 +7,7 @@
 
 ## REMAPS
 vim.api.nvim_set_keymap({mode}, {keymap}, {mapped to}, {options})
+Para ver en que se usa cierto remap :verbose {mode}map {keymap}
 
 ## PACKAGES
 Packer
@@ -19,13 +20,29 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 >> :PackerSync
 
 ### Interesting packages
-* Aerial -> (No instalado) barra lateral para moverse mas rapido por el doc, ver funciones, clases, etc
-* vim-bufferline -> (No instalado) muestra los buffers abiertos y si estan guardados
-* neotree -> revisar mas en detalle
+#### LSP
+Open with :LspInstallInfo
+Show information in current buffer :LspInfo
 
+### SNIPETS
+When appears ${n: name} means that has supertab. The tab can be used to jump between arguments
+As in autocomplete it useful <c-e> and <c-y>
+There is a lot of possible sources of completion (see docs)
 
-
-
+### VIM dap
+#### For python
+1- Install debugpy
+2- It is recommended to install debugpy into a dedicated virtualenv. To do so:
+```bash
+mkdir .virtualenvs
+cd .virtualenvs
+python -m venv debugpy
+debugpy/bin/python -m pip install debugpy 
+```
+3- Install treesitter extension
+```
+:TSInstall python
+```
 
 
 
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,23 @@
 
 require('plugins')
-require('options')
-require('keymaps')
-
-
--- Comands --------------------------------------------------------------
-vim.api.nvim_command('set nohlsearch')
-vim.api.nvim_command('highlight WinSeparator guibg=None')
 
 
 -- Colors -------------------------------------------------------------
 vim.cmd[[colorscheme nord]]
+vim.api.nvim_command('set nohlsearch')
+vim.api.nvim_command('set fillchars+=vert:│')  -- Set icon to separate windows
+vim.cmd[[hi vertsplit guifg=fg guibg=bg]]  -- Set colors for window separation
 
--- Plugins
-require('plugins/cmd')
+
+-- Plugins --------------------------------------------------------------
+require('plugins/cmp')
 require('plugins/telescope')
+require('telescope').load_extension "file_browser"
 require('plugins/treesitter')
 require('plugins/autopairs')
 require('plugins/comment')
 require('plugins/gitsigns')
--- require('plugins/nvim-tree')
-require('plugins/neo-tree')
+-- require('plugins/neo-tree')
 require('plugins/bufferline')
 require('plugins/lualine')
 require('plugins/impatient')
@@ -29,4 +26,21 @@ require('plugins/alpha')
 require('plugins/whichkey')
 require('plugins/project')
 require('plugins/lsp')
+require('plugins/notify')
+require('plugins/colorizer')
+require('plugins/dap_python')
+require('plugins/dap_ui')
 
+require'lspconfig'.pyright.setup{}
+
+
+-- Final options and keymaps ---------------------------------------------
+require('options')
+require('keymaps')
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<space>fb",
+  ":Telescope file_browser",
+  { noremap = true }
+)
