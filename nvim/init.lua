2f646a01b53f790df09172de02a4c9f9feb129e2
sergio <sergiocordonluis@gmail.com>
2024-01-23 14:06:57 +0100

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -125,7 +125,24 @@ vim.o.ch = 0  -- space for command line at the end
 
 
 
+if vim.g.vscode then
+  -- Movement over folds
+  local function moveCursor(direction)
+    if (vim.fn.reg_recording() == '' and vim.fn.reg_executing() == '') then
+      return ('g' .. direction)
+    else
+      return direction
+    end
+  end
 
+  vim.keymap.set('n', 'k', function()
+    return moveCursor('k')
+  end, { expr = true, remap = true })
+  vim.keymap.set('n', 'j', function()
+    return moveCursor('j')
+  end, { expr = true, remap = true })
+
+end
 
 
 
